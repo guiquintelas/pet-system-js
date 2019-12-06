ce61fd2a649e8a8edd8fb3b1d58997bec7913235
Guilherme Frota <guiquintelas96@gmail.com>
2019-12-06 19:21:23 -0300

    add PetTable params

--- tp3/pages/Usuarios.js
+++ tp3/pages/Usuarios.js
@@ -1,10 +1,21 @@
 class Usuario extends PetPage {
     getBody() {
         return /*template*/`
-            <pet-table items="usuarios">
-            </pet-table>
+            <pet-table 
+                items="usuarios"
+                item-slot="userTable"
+            ></pet-table>
         `
     }
+}
+
+function userTable(item) {
+    return /* template */`
+        <td data-name="ID">${item.id}</td>
+        <td data-name="Nome">${item.name}</td>
+        <td data-name="Email">${item.email}</td>
+        <td data-name="Senha">${item.password}</td>
+    `
 }
   
 window.customElements.define('pet-usuarios', Usuario);
